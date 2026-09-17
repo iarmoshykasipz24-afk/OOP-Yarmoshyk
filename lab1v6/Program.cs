ce7db27cfcac31c94b760d28088ba0455cde68f3
iarmoshykasipz24-afk <iarmoshyk.a.s_ipz24@rcit.ukr.education>
2026-09-17 14:17:00 +0300

    Initial commit

---
+++ lab1v6/Program.cs
@@ -0,0 +1,45 @@
+﻿BankAccount account1 = new BankAccount("Іван Петренко", "UA001", 1000);
+BankAccount account2 = new BankAccount("Марія Коваль", "UA002", 500);
+BankAccount account3 = new BankAccount("Олег Сидоренко", "UA003", 2000);
+
+account1.Deposit(500);
+account1.Withdraw(200);
+Console.WriteLine($"Рахунок {account1.Balance}");
+
+account2.Deposit(1000);
+account2.Withdraw(2000);
+Console.WriteLine($"Рахунок {account2.Balance}");
+
+account3.Withdraw(500);
+Console.WriteLine($"Рахунок {account3.Balance}");
+
+class BankAccount
+{
+    private string _owner;
+    private string _accountNumber;
+    public decimal Balance { get; set; }
+
+    public BankAccount(string owner, string accountNumber, decimal startBalance)
+    {
+        _owner = owner;
+        _accountNumber = accountNumber;
+        Balance = startBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance = Balance + amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance = Balance - amount;
+        }
+        else
+        {
+            Console.WriteLine("Недостатньо коштів на рахунку");
+        }
+    }
+}
